78615c254e903392e077914f47dfdfb0b7374b0e
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-06-29 22:52:39 +0400

    Added task model

---
+++ src/main/java/com/example/todolistapi/model/Task.java
@@ -0,0 +1,37 @@
+public class Task {
+    private int id;
+    private String title;
+    private boolean isCompleted;
+
+    public Task() {}
+
+    public Task(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.isCompleted = false;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public boolean getCompleted() {
+        return this.isCompleted;
+    }
+}
