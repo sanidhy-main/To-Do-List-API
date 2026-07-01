f07a0707b3a1d9875d6c793e385570abe3ec89e2
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-07-01 21:31:49 +0400

    Added customisation

--- src/main/java/com/example/todolistapi/ToDoListApiApplication.java
+++ src/main/java/com/example/todolistapi/ToDoListApiApplication.java
@@ -3,7 +3,23 @@ package com.example.todolistapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+OpenAPIDefinition(
+        info = @Info(
+                title = "Sanidhy's To-Do API",
+        version = "1.0.0",
+        description = "A REST API for managing tasks.",
+        contact = @Contact(
+                name = "Sanidhy Chaturvedi",
+        url = "https://github.com/sanidhy-main"
+)
+    )
+            )
+
 public class ToDoListApiApplication {
 
     public static void main(String[] args) {
